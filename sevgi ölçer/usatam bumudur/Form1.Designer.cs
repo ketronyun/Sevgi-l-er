7f20683e734a9f4cdb298dc4d21b0ec8f8c72752
Yunus Emre Oflaz <91126924+ketronyun@users.noreply.github.com>
2025-05-16 16:45:36 +0300

    Add files via upload

---
+++ sevgi ölçer/usatam bumudur/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace usatam_bumudur
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(262, 315);
+            button1.Name = "button1";
+            button1.Size = new Size(160, 51);
+            button1.TabIndex = 3;
+            button1.Text = "Sorgula";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(90, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Senin İsmin";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(99, 137);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Onun İsmi";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(74, 84);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(74, 160);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(255, 128, 255);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Location = new Point(117, 256);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(465, 20);
+            textBox3.TabIndex = 8;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(255, 128, 255);
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Location = new Point(117, 223);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(332, 20);
+            textBox4.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(414, 42);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(227, 189);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 255);
+            ClientSize = new Size(718, 420);
+            Controls.Add(pictureBox1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Sevgi ölçer";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ sevgi ölçer/usatam bumudur/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Forms;
+
+namespace usatam_bumudur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string isim1 = textBox1.Text;
+            string isim2 = textBox2.Text;
+            string cevap = textBox3.Text;
+            string yuzde = textBox4.Text;
+
+
+
+            Random rnd = new Random();
+            int sayi = rnd.Next(0, 100);
+
+            if (textBox1.Text == "emre")
+            {
+                sayi = 9999999;
+            }
+
+
+            yuzde = $"Seni %{sayi} kadar seviyor yani";
+
+            if (sayi > 90) {
+                cevap = "seni nasıl sevdiğini anlatamam bile ☻";
+                pictureBox1.Image = Image.FromFile("C:\\Users\\k3tr0n\\Desktop\\sevgi ölçer\\usatam bumudur\\resimler\\90.jpg");
+            }
+
+
+            else if (sayi > 75) {
+                cevap = "Seni çok seviyor";
+                pictureBox1.Image = Image.FromFile("C:\\Users\\k3tr0n\\Desktop\\sevgi ölçer\\usatam bumudur\\resimler\\75.jpg");
+            }
+
+
+            else if (sayi > 50) {
+                cevap = "Seni arkadaşça seviyor";
+                pictureBox1.Image = Image.FromFile("C:\\Users\\k3tr0n\\Desktop\\sevgi ölçer\\usatam bumudur\\resimler\\50.jpg");
+            }
+
+            else if (sayi > 25) { 
+            cevap = "Görürse selam verir anca";
+            pictureBox1.Image = Image.FromFile("C:\\Users\\k3tr0n\\Desktop\\sevgi ölçer\\usatam bumudur\\resimler\\25.jpg");
+             }
+
+            else { 
+                cevap = "Yazman hata seni hiç sevmiyor";
+                pictureBox1.Image = Image.FromFile("C:\\Users\\k3tr0n\\Desktop\\sevgi ölçer\\usatam bumudur\\resimler\\25.jpg");
+             }
+
+            textBox3.Text = cevap;
+            textBox4 .Text = yuzde;
+
+
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
